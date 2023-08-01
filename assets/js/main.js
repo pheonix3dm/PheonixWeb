eec0588232171123f7c02720bb4213fa871a9271
seddharth <67575587+seddharth@users.noreply.github.com>
2023-08-01 14:16:07 +0530

    contact form js

--- assets/js/main.js
+++ assets/js/main.js
@@ -272,3 +272,17 @@
   new PureCounter();
 
 })()
+
+
+/* Google */
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx7gUqCxudGSYAPN2QGd47hj2KgVIlaM5F-Zo1Nd960Jj_GXSkC3vVgT-7MsvmZXdvq/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
